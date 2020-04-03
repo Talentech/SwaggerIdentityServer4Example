7a9b1cc51cca88f5aa452388d95899b28c3be409
Tomasz Margalski <tomasz.margalski@hotmail.com>
2020-04-03 16:25:15 +0200

    authorize check filter & security definition on swagger

---
+++ SwaggerIdentityServer4Example/AuthorizeCheckOperationFilter.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SwaggerIdentityServer4Example
+{
+    public class AuthorizeCheckOperationFilter : IOperationFilter
+    {
+        public void Apply(OpenApiOperation operation, OperationFilterContext context)
+        {
+            var allowAnonymous = context.MethodInfo
+                .GetCustomAttributes(true)
+                .OfType<AllowAnonymousAttribute>()
+                .Distinct();
+
+            if (allowAnonymous.Any()) 
+                return; 
+
+            operation.Responses.Add("401", new OpenApiResponse { Description = "Unauthorized" });
+            operation.Responses.Add("403", new OpenApiResponse { Description = "Forbidden" });
+            var oAuthScheme = new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference { Type = ReferenceType.SecurityScheme, Id = "oauth2" }
+            };
+            operation.Security = new List<OpenApiSecurityRequirement>
+            {
+                new OpenApiSecurityRequirement
+                {
+                    [ oAuthScheme ] = new[] { "demo-test-api-scope" }
+                }
+            };
+        }
+    }
+}
--- SwaggerIdentityServer4Example/Startup.cs
+++ SwaggerIdentityServer4Example/Startup.cs
@@ -5,6 +5,8 @@ using Microsoft.AspNetCore.Mvc.Authorization;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.OpenApi.Models;
+using System;
+using System.Collections.Generic;
 
 namespace SwaggerIdentityServer4Example
 {
@@ -33,6 +35,25 @@ namespace SwaggerIdentityServer4Example
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
+                c.AddSecurityDefinition("oauth2", new OpenApiSecurityScheme
+                {
+                    Type = SecuritySchemeType.OAuth2,
+                    Flows = new OpenApiOAuthFlows
+                    {
+                        Implicit = new OpenApiOAuthFlow
+                        {
+                            AuthorizationUrl = new Uri("https://localhost:44333/connect/authorize"),
+                            TokenUrl = new Uri("https://localhost:44333/connect/token"),
+                            Scopes = new Dictionary<string, string>()
+                            {
+                                {
+                                    "demo-test-api-scope", "Demo API Swagger Auth Example"
+                                }
+                            },
+                        }
+                    }
+                });
+                c.OperationFilter<AuthorizeCheckOperationFilter>();
             });
         }
 
@@ -45,6 +66,8 @@ namespace SwaggerIdentityServer4Example
             app.UseSwaggerUI(c =>
             {
                 c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+
+                c.OAuthClientId("mvc-test-api-swagger-app");
             });
 
             app.UseRouting();
@@ -59,7 +82,7 @@ namespace SwaggerIdentityServer4Example
             services.AddAuthentication(IdentityServerAuthenticationDefaults.AuthenticationScheme)
                 .AddIdentityServerAuthentication(options =>
                 {
-                    options.Authority = "http://localhost:5000";  // identity server endpoint
+                    options.Authority = "https://localhost:44333";  // identity server endpoint
                     options.ApiName = "demo-test-api-scope";
                     options.RequireHttpsMetadata = false; // this should be true on production
                 });
