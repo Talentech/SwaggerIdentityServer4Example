2511788b8020d415cac0d0f22872c07fed296a26
Tomasz Margalski <tomasz.margalski@hotmail.com>
2020-04-03 11:46:25 +0200

    global authentication filter

--- SwaggerIdentityServer4Example/Startup.cs
+++ SwaggerIdentityServer4Example/Startup.cs
@@ -1,4 +1,7 @@
+using IdentityServer4.AccessTokenValidation;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Mvc.Authorization;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.OpenApi.Models;
@@ -12,21 +15,33 @@ namespace SwaggerIdentityServer4Example
             Configuration = configuration;
         }
         public IConfiguration Configuration { get; }
-         
+
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc(options =>
+            {
+                options.Filters.Add(new AuthorizeFilter(new AuthorizationPolicyBuilder()
+                    .RequireAuthenticatedUser()
+                    .AddAuthenticationSchemes(IdentityServerAuthenticationDefaults.AuthenticationScheme)
+                    .Build()));
+            });
+
             services.AddControllers();
-             
+
+            ConfigureAuth(services);
+
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
             });
         }
-         
+
         public void Configure(IApplicationBuilder app)
-        { 
+        {
+            app.UseAuthentication();
+
             app.UseSwagger();
-             
+
             app.UseSwaggerUI(c =>
             {
                 c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
@@ -37,6 +52,17 @@ namespace SwaggerIdentityServer4Example
             {
                 endpoints.MapControllers();
             });
+        }
+
+        protected void ConfigureAuth(IServiceCollection services)
+        {
+            services.AddAuthentication(IdentityServerAuthenticationDefaults.AuthenticationScheme)
+                .AddIdentityServerAuthentication(options =>
+                {
+                    options.Authority = "http://localhost:5000";  // identity server endpoint
+                    options.ApiName = "demo-test-api-scope";
+                    options.RequireHttpsMetadata = false; // this should be true on production
+                });
         }
     }
 }
